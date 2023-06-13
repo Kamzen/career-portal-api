dbba5de8dc2a829ffc5e360553b9c6b2ecbfbdf5
Kamzen <kamzen1994@gmail.com>
2023-06-13 17:20:46 +0200

    Update learnerprogramme.js

--- models/learnerprogramme.js
+++ models/learnerprogramme.js
@@ -7,8 +7,17 @@ module.exports = (sequelize, DataTypes) => {
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
-    static associate(models) {
+    static associate({ User, Programme }) {
       // define association here
+
+      this.belongsTo(User, {
+        foreignKey: "userId",
+      });
+
+      this.belongsTo(Programme, {
+        foreignKey: "programmeId",
+        as: "programmes",
+      });
     }
   }
   LearnerProgramme.init(
