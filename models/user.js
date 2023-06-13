91e8ddb08db24988ef46b2b0efc25ead3c8ee484
Kamzen <kamzen1994@gmail.com>
2023-06-13 18:57:41 +0200

    Update user.js

--- models/user.js
+++ models/user.js
@@ -75,16 +75,20 @@ module.exports = (sequelize, DataTypes) => {
       },
       firstName: {
         type: DataTypes.TEXT,
+        allowNull: false
       },
       lastName: {
         type: DataTypes.TEXT,
+        allowNull: false
       },
       middleName: {
         type: DataTypes.TEXT,
+        allowNull: false
       },
       email: {
         type: DataTypes.TEXT,
         allowNull: false,
+        unique: true
       },
       password: {
         type: DataTypes.TEXT,
@@ -93,6 +97,9 @@ module.exports = (sequelize, DataTypes) => {
       userType: {
         type: DataTypes.TEXT,
         allowNull: false,
+      },
+      resetPasswordToken: {
+        type: DataTypes.TEXT
       },
       dateCreated: {
         allowNull: false,
