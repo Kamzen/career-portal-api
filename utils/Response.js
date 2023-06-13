98685542531f6ef462ca811eeba95b626bed4dce
Kamzen <kamzen1994@gmail.com>
2023-06-13 16:59:37 +0200

    Create Response.js

---
+++ utils/Response.js
@@ -0,0 +1,20 @@
+class ApiError extends Error {
+  constructor(message, code) {
+    super(message);
+    this.message = message || "Server Error";
+    this.errorCode = code || 5000;
+  }
+}
+
+const ApiResp = (message, key, data) => {
+  return {
+    success: true,
+    message: message,
+    [key]: data,
+  };
+};
+
+module.exports = {
+  ApiError,
+  ApiResp,
+};
