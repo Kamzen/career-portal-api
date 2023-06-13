87fff91179f759501465e7563260ac713b8deef8
Kamzen <kamzen1994@gmail.com>
2023-06-13 18:58:02 +0200

    Update ErrorMid.js

--- middlewares/ErrorMid.js
+++ middlewares/ErrorMid.js
@@ -1,18 +1,20 @@
 const { ApiError } = require("../utils/Response");
 
-
 const ErrorMid = (err, req, res, next) => {
   if (err instanceof ApiError) {
     return res.status(err.errorCode).json({
       success: false,
       message: err.message,
       name: err.name,
+      error: err,
     });
   }
 
   return res.status(500).json({
     success: false,
     message: "Server Error",
+    name: err.name,
+    error: err,
   });
 };
 
