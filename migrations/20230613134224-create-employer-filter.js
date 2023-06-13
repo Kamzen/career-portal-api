84ba9de656c1e8879004436334d8433ae024714c
Kamzen <kamzen1994@gmail.com>
2023-06-13 17:00:55 +0200

    Create 20230613134224-create-employer-filter.js

---
+++ migrations/20230613134224-create-employer-filter.js
@@ -0,0 +1,31 @@
+"use strict";
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, DataTypes) {
+    await queryInterface.createTable("employerFilters", {
+      id: {
+        allowNull: false,
+        primaryKey: true,
+        type: DataTypes.UUID,
+      },
+      filters: {
+        type: DataTypes.ARRAY(DataTypes.JSONB),
+      },
+      userId: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+      },
+      dateCreated: {
+        allowNull: false,
+        type: DataTypes.DATE,
+      },
+      dateUpdated: {
+        allowNull: false,
+        type: DataTypes.DATE,
+      },
+    });
+  },
+  async down(queryInterface, DataTypes) {
+    await queryInterface.dropTable("employerFilters");
+  },
+};
