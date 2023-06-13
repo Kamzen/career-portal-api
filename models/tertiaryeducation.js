6e3af5bdf36c16e8228d1de6bfb2ac6735d214c0
Kamzen <kamzen1994@gmail.com>
2023-06-13 16:59:54 +0200

    Create tertiaryeducation.js

---
+++ models/tertiaryeducation.js
@@ -0,0 +1,64 @@
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class TertiaryEducation extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  TertiaryEducation.init(
+    {
+      id: {
+        allowNull: false,
+        primaryKey: true,
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+      },
+      educationLevel: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+      },
+      fieldOfStudy: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+      },
+      startYear: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+      },
+      endYear: {
+        type: DataTypes.TEXT,
+      },
+      status: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+      },
+      userId: {
+        type: DataTypes.UUID,
+        allowNull: false,
+      },
+      dateCreated: {
+        allowNull: false,
+        type: DataTypes.DATE,
+      },
+      dateUpdated: {
+        allowNull: false,
+        type: DataTypes.DATE,
+      },
+    },
+    {
+      sequelize,
+      modelName: "TertiaryEducation",
+      tableName: "tertiaryEducations",
+      timestamps: true,
+      createdAt: "dateCreated",
+      updatedAt: "dateUpdated",
+    }
+  );
+  return TertiaryEducation;
+};
