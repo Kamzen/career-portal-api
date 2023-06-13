1e132177c2079e584332c1b168ae6eecc5bb420b
Kamzen <kamzen1994@gmail.com>
2023-06-13 17:00:17 +0200

    Create learnerprogramme.js

---
+++ models/learnerprogramme.js
@@ -0,0 +1,49 @@
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class LearnerProgramme extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  LearnerProgramme.init(
+    {
+      id: {
+        allowNull: false,
+        primaryKey: true,
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+      },
+      userId: {
+        type: DataTypes.UUID,
+        allowNull: false,
+      },
+      programmeId: {
+        type: DataTypes.UUID,
+        allowNull: false,
+      },
+      dateCreated: {
+        allowNull: false,
+        type: DataTypes.DATE,
+      },
+      dateUpdated: {
+        allowNull: false,
+        type: DataTypes.DATE,
+      },
+    },
+    {
+      sequelize,
+      modelName: "LearnerProgramme",
+      tableName: "learnerProgrammes",
+      timestamps: true,
+      createdAt: "dateCreated",
+      updatedAt: "dateUpdated",
+    }
+  );
+  return LearnerProgramme;
+};
