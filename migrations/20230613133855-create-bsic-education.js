b10d322434ed8be29aa512596ec26987b10abdd1
Kamzen <kamzen1994@gmail.com>
2023-06-13 17:01:37 +0200

    Create 20230613133855-create-bsic-education.js

---
+++ migrations/20230613133855-create-bsic-education.js
@@ -0,0 +1,44 @@
+"use strict";
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, DataTypes) {
+    await queryInterface.createTable("basicEducations", {
+      id: {
+        allowNull: false,
+        primaryKey: true,
+        type: DataTypes.UUID,
+      },
+      grade: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+      },
+      school: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+      },
+      city: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+      },
+      provice: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+      },
+      userId: {
+        type: DataTypes.UUID,
+        allowNull: false,
+      },
+      dateCreated: {
+        allowNull: false,
+        type: DataTypes.DATE,
+      },
+      dateUpdated: {
+        allowNull: false,
+        type: DataTypes.DATE,
+      },
+    });
+  },
+  async down(queryInterface, DataTypes) {
+    await queryInterface.dropTable("basicEducations");
+  },
+};
