048e00c00fe77b93c2a2a1fe382f0931ce03714b
Kamzen <kamzen1994@gmail.com>
2023-06-13 17:00:06 +0200

    Create programme.js

---
+++ models/programme.js
@@ -0,0 +1,49 @@
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class Programme extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Programme.init(
+    {
+      id: {
+        allowNull: false,
+        primaryKey: true,
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4
+      },
+      title: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+      },
+      description: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+      },
+      dateCreated: {
+        allowNull: false,
+        type: DataTypes.DATE,
+      },
+      dateUpdated: {
+        allowNull: false,
+        type: DataTypes.DATE,
+      },
+    },
+    {
+      sequelize,
+      modelName: "Programme",
+      tableName: "programmes",
+      timestamps: true,
+      createdAt: "dateCreated",
+      updatedAt: 'dateUpdated',
+    }
+  );
+  return Programme;
+};
