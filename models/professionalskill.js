bf6ee1f026fe704e9f9b215c8575e8c20ce4b5b2
Kamzen <kamzen1994@gmail.com>
2023-06-13 17:00:11 +0200

    Create professionalskill.js

---
+++ models/professionalskill.js
@@ -0,0 +1,53 @@
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class ProfessionalSkill extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  ProfessionalSkill.init(
+    {
+      id: {
+        allowNull: false,
+        primaryKey: true,
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+      },
+      skill: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+      },
+      skillLevel: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+      },
+      userId: {
+        type: DataTypes.UUID,
+        allowNull: false,
+      },
+      dateCreated: {
+        allowNull: false,
+        type: DataTypes.DATE,
+      },
+      dateUpdated: {
+        allowNull: false,
+        type: DataTypes.DATE,
+      },
+    },
+    {
+      sequelize,
+      modelName: "ProfessionalSkill",
+      tableName: "professionalSkills",
+      timestamps: true,
+      createdAt: "dateCreated",
+      updatedAt: "dateUpdated",
+    }
+  );
+  return ProfessionalSkill;
+};
