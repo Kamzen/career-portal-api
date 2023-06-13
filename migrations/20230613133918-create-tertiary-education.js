a2040050028318e81e3950b6eeac2c1512045e06
Kamzen <kamzen1994@gmail.com>
2023-06-13 17:01:30 +0200

    Create 20230613133918-create-tertiary-education.js

---
+++ migrations/20230613133918-create-tertiary-education.js
@@ -0,0 +1,47 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, DataTypes) {
+    await queryInterface.createTable('tertiaryEducations', {
+      id: {
+        allowNull: false,
+        primaryKey: true,
+        type: DataTypes.UUID,
+      },
+      educationLevel: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+      },
+      fieldOfStudy: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+      },
+      startYear: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+      },
+      endYear: {
+        type: DataTypes.TEXT,
+      },
+      status: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+      },
+      userId: {
+        type: DataTypes.UUID,
+        allowNull: false,
+      },
+      dateCreated: {
+        allowNull: false,
+        type: DataTypes.DATE,
+      },
+      dateUpdated: {
+        allowNull: false,
+        type: DataTypes.DATE,
+      },
+    });
+  },
+  async down(queryInterface, DataTypes) {
+    await queryInterface.dropTable('tertiaryEducations');
+  }
+};
