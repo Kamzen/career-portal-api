02287148cfeddf19a0995fe06707a1b0622bc545
Kamzen <kamzen1994@gmail.com>
2023-06-13 18:57:52 +0200

    Update 20230613133805-create-student-information.js

--- migrations/20230613133805-create-student-information.js
+++ migrations/20230613133805-create-student-information.js
@@ -11,22 +11,23 @@ module.exports = {
       identificationNumber: {
         type: DataTypes.TEXT,
         allowNull: false,
+        unique: true
       },
       disbility: {
         type: DataTypes.TEXT,
-        allowNull: false,
+        // allowNull: false,
       },
       careerStatus: {
         type: DataTypes.ARRAY(DataTypes.TEXT),
-        allowNull: false,
+        // allowNull: false,
       },
       mobileNumber: {
         type: DataTypes.TEXT,
-        allowNull: false,
+        // allowNull: false,
       },
       race: {
         type: DataTypes.TEXT,
-        allowNull: false,
+        // allowNull: false,
       },
       userId: {
         type: DataTypes.UUID,
