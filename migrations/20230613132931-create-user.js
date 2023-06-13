ff82850e4d2fad6c2ae76d26589fd11bc0d588f6
Kamzen <kamzen1994@gmail.com>
2023-06-13 17:02:33 +0200

    All models and migrations created and api error middleware implemented

---
+++ migrations/20230613132931-create-user.js
@@ -0,0 +1,45 @@
+"use strict";
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, DataTypes) {
+    await queryInterface.createTable("users", {
+      id: {
+        allowNull: false,
+        primaryKey: true,
+        type: DataTypes.UUID,
+      },
+      firstName: {
+        type: DataTypes.TEXT,
+      },
+      lastName: {
+        type: DataTypes.TEXT,
+      },
+      middleName: {
+        type: DataTypes.TEXT,
+      },
+      email: {
+        type: DataTypes.TEXT,
+        allowNull: false
+      },
+      password: {
+        type: DataTypes.TEXT,
+        allowNull: false
+      },
+      userType: {
+        type: DataTypes.TEXT,
+        allowNull: false
+      },
+      dateCreated: {
+        allowNull: false,
+        type: DataTypes.DATE,
+      },
+      dateUpdated: {
+        allowNull: false,
+        type: DataTypes.DATE,
+      },
+    });
+  },
+  async down(queryInterface, DataTypes) {
+    await queryInterface.dropTable("users");
+  },
+};
