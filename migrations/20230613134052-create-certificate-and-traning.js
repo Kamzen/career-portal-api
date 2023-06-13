54d54118d57fd52f79568202f8130fef74acaba9
Kamzen <kamzen1994@gmail.com>
2023-06-13 17:01:14 +0200

    Create 20230613134052-create-certificate-and-traning.js

---
+++ migrations/20230613134052-create-certificate-and-traning.js
@@ -0,0 +1,36 @@
+"use strict";
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, DataTypes) {
+    await queryInterface.createTable("certificateAndTranings", {
+      id: {
+        allowNull: false,
+        primaryKey: true,
+        type: DataTypes.UUID,
+      },
+      course: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+      },
+      year: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+      },
+      userId: {
+        type: DataTypes.UUID,
+        allowNull: false,
+      },
+      dateCreated: {
+        allowNull: false,
+        type: DataTypes.DATE,
+      },
+      dateUpdated: {
+        allowNull: false,
+        type: DataTypes.DATE,
+      },
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable("certificateAndTranings");
+  },
+};
