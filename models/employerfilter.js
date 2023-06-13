517f6d13b644de40f04f923f48a636ac988d7c53
Kamzen <kamzen1994@gmail.com>
2023-06-13 17:00:22 +0200

    Create employerfilter.js

---
+++ models/employerfilter.js
@@ -0,0 +1,48 @@
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class EmployerFilter extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  EmployerFilter.init(
+    {
+      id: {
+        allowNull: false,
+        primaryKey: true,
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4
+      },
+      filters: {
+        type: DataTypes.ARRAY(DataTypes.JSONB),
+      },
+      userId: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+      },
+      dateCreated: {
+        allowNull: false,
+        type: DataTypes.DATE,
+      },
+      dateUpdated: {
+        allowNull: false,
+        type: DataTypes.DATE,
+      },
+    },
+    {
+      sequelize,
+      modelName: "EmployerFilter",
+      tableName: "employerFilters",
+      timestamps: true,
+      createdAt: "dateCreated",
+      updatedAt: "dateUpdated",
+    }
+  );
+  return EmployerFilter;
+};
