dcc2409e64bef6d1f53ba1595579594e0c1a9f54
Kamzen <kamzen1994@gmail.com>
2023-06-13 17:01:43 +0200

    Create 20230613133830-create-address.js

---
+++ migrations/20230613133830-create-address.js
@@ -0,0 +1,58 @@
+"use strict";
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, DataTypes) {
+    await queryInterface.createTable("addresses", {
+      id: {
+        allowNull: false,
+        primaryKey: true,
+        type: DataTypes.UUID,
+      },
+      streetNumber: {
+        type: DataTypes.TEXT,
+      },
+      streetName: {
+        type: DataTypes.TEXT,
+      },
+      suburb: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+      },
+      manicipality: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+      },
+      city: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+      },
+      province: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+      },
+      country: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+      },
+      postalCode: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+      },
+      userId: {
+        type: DataTypes.UUID,
+        allowNull: false,
+      },
+      dateCreated: {
+        allowNull: false,
+        type: DataTypes.DATE,
+      },
+      dateUpdated: {
+        allowNull: false,
+        type: DataTypes.DATE,
+      },
+    });
+  },
+  async down(queryInterface, DataTypes) {
+    await queryInterface.dropTable("addresses");
+  },
+};
