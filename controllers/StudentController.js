ac984b19cac3fc0ec5dff941e40bdcb47bf35e22
Kamzen <kamzen1994@gmail.com>
2023-06-14 20:49:00 +0200

    Update StudentController.js

--- controllers/StudentController.js
+++ controllers/StudentController.js
@@ -1,14 +1,46 @@
+const { Op } = require('sequelize');
+const { Address } = require('../models');
+const { ApiError, ApiResp } = require('../utils/Response');
+
 const StudentController = {
 
   editStudentBasicInformation: (req, res, next) => {
     
   },
 
-  addStudentAddress: (req, res, next) => {
+  addStudentAddress: async (req, res, next) => {
+
+    try{
+
+      const address = await Address.create({...req.body, userId: req.user.id});
+
+      if(!address) throw new ApiError('Error saving address', 404);
+
+      return res.status(201).json(ApiResp('Address saved successfully'))
+
+    }catch(e){
+      console.log(e)
+      next(e)
+    }
 
   },
 
-  editStudentAddress: (req, res, next) => {
+  editStudentAddress: async (req, res, next) => {
+
+    const { addressId } = req.params;
+
+    const address = await Address.findOne({
+      where: {[Op.and]: [
+        { id: addressId },
+        { userId: req.user.id }
+      ]}
+    })
+
+    if(!address) throw new ApiError('Address edit error', 404);
+
+    await address.update({...req.body});
+
+    return res.status(200).json(ApiResp('Address updated successfully'))
 
   },
   
