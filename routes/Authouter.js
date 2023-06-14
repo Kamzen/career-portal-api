9eb0194704fb5a4c783b4d6bdc63d3abde237baf
Kamzen <kamzen1994@gmail.com>
2023-06-14 16:52:35 +0200

    Update Authouter.js

--- routes/Authouter.js
+++ routes/Authouter.js
@@ -1,12 +1,13 @@
 const { Router } = require("express");
 const AuthController = require("../controllers/AuthController");
+const AuthMid = require("../middlewares/AuthMid");
 
 const AuthRouter = Router();
 
 AuthRouter.post("/login", AuthController.signInUser);
 AuthRouter.post("/register", AuthController.signUpUser);
 AuthRouter.post('/resetPassword', AuthController.resetPasswordUser)
-AuthRouter.get('/isUserLoggedIn', AuthController.isUserLoggedIn)
+AuthRouter.get('/isUserLoggedIn', AuthMid, AuthController.isUserLoggedIn)
 AuthRouter.get('/refreshToken', AuthController.refreshToken)
 
 
