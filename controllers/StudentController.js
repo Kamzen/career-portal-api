38b7c27a6d2005b2c85a06727c08cb5cfe3b43dc
Kamzen <kamzen1994@gmail.com>
2023-06-14 16:52:45 +0200

    Create StudentController.js

---
+++ controllers/StudentController.js
@@ -0,0 +1,17 @@
+const StudentController = {
+
+  editStudentBasicInformation: (req, res, next) => {
+    
+  },
+
+  addStudentAddress: (req, res, next) => {
+
+  },
+
+  editStudentAddress: (req, res, next) => {
+
+  },
+  
+}
+
+module.exports = StudentController;
