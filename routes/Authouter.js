2e4bd0f0aad76c82ef12d3323f2d4ed75f68ddd3
Kamzen <kamzen1994@gmail.com>
2023-06-14 10:53:09 +0200

    Update Authouter.js

--- routes/Authouter.js
+++ routes/Authouter.js
@@ -5,7 +5,9 @@ const AuthRouter = Router();
 
 AuthRouter.post("/login", AuthController.signInUser);
 AuthRouter.post("/register", AuthController.signUpUser);
-AuthRouter.post('/reset-password', AuthController.resetPasswordUser)
+AuthRouter.post('/resetPassword', AuthController.resetPasswordUser)
+AuthRouter.get('/isUserLoggedIn', AuthController.isUserLoggedIn)
+AuthRouter.get('/refreshToken', AuthController.refreshToken)
 
 
 module.exports = AuthRouter;
