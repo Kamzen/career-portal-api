21f24ea1dc1b86dad73d75d75c36adca106b3561
Kamzen <kamzen1994@gmail.com>
2023-06-19 12:17:12 +0200

    Update address.js

--- models/address.js
+++ models/address.js
@@ -56,6 +56,9 @@ module.exports = (sequelize, DataTypes) => {
         type: DataTypes.UUID,
         allowNull: false,
       },
+      completed:{
+        type: DataTypes.BOOLEAN,
+      },
       dateCreated: {
         allowNull: false,
         type: DataTypes.DATE,
