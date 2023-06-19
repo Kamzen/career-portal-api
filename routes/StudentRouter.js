d44c2d846f78588a4180fc0e04f1327b54e7f654
Kamzen <kamzen1994@gmail.com>
2023-06-19 12:16:49 +0200

    Update StudentRouter.js

--- routes/StudentRouter.js
+++ routes/StudentRouter.js
@@ -1,4 +1,4 @@
-const {Router} = require('express');
+const { Router } = require('express');
 const StudentController = require('../controllers/StudentController');
 const AuthMid = require('../middlewares/AuthMid');
 
