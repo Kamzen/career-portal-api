f3ccf68e8a790a4c0d81db660e98c78d6135cdba
Kamzen <kamzen1994@gmail.com>
2023-06-19 12:17:23 +0200

    Update 20230613133918-create-tertiary-education.js

--- migrations/20230613133918-create-tertiary-education.js
+++ migrations/20230613133918-create-tertiary-education.js
@@ -1,47 +1,54 @@
-'use strict';
+"use strict";
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, DataTypes) {
-    await queryInterface.createTable('tertiaryEducations', {
+    await queryInterface.createTable("tertiaryEducations", {
       id: {
         allowNull: false,
         primaryKey: true,
-        type: DataTypes.UUID,
+        type: DataTypes.UUID
       },
       educationLevel: {
         type: DataTypes.TEXT,
-        allowNull: false,
+        allowNull: false
       },
       fieldOfStudy: {
         type: DataTypes.TEXT,
-        allowNull: false,
+        allowNull: false
+      },
+      institution: {
+        type: DataTypes.TEXT,
+        allowNull: false
       },
       startYear: {
         type: DataTypes.TEXT,
-        allowNull: false,
+        allowNull: false
       },
       endYear: {
-        type: DataTypes.TEXT,
+        type: DataTypes.TEXT
       },
       status: {
         type: DataTypes.TEXT,
-        allowNull: false,
+        allowNull: false
       },
       userId: {
         type: DataTypes.UUID,
-        allowNull: false,
+        allowNull: false
+      },
+      completed: {
+        type: DataTypes.BOOLEAN
       },
       dateCreated: {
         allowNull: false,
-        type: DataTypes.DATE,
+        type: DataTypes.DATE
       },
       dateUpdated: {
         allowNull: false,
-        type: DataTypes.DATE,
-      },
+        type: DataTypes.DATE
+      }
     });
   },
   async down(queryInterface, DataTypes) {
-    await queryInterface.dropTable('tertiaryEducations');
+    await queryInterface.dropTable("tertiaryEducations");
   }
 };
