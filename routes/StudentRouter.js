6769782fbd91182db51f58b98f20259c3cfbcbe7
Kamzen <kamzen1994@gmail.com>
2023-06-19 11:23:57 +0200

    Update StudentRouter.js

--- routes/StudentRouter.js
+++ routes/StudentRouter.js
@@ -6,5 +6,7 @@ const StudentRouter = Router();
 
 StudentRouter.post('/addAddress', AuthMid, StudentController.addStudentAddress);
 StudentRouter.post('/editAddress/:addressId', AuthMid, StudentController.editStudentAddress);
+StudentRouter.post('/editBasicInformation/:userId', AuthMid, StudentController.editStudentBasicInformation);
+StudentRouter.post('/addBasicEducation', AuthMid, StudentController.addBasicEducation);
 
 module.exports = StudentRouter;
