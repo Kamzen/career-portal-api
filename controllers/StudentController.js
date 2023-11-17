5f3b9a36b5c014e431ba8a6b68cf8dc106d431ca
Kamzen <kamzen1994@gmail.com>
2023-11-17 10:42:25 +0200

    Update StudentController.js

--- controllers/StudentController.js
+++ controllers/StudentController.js
@@ -1,58 +1,54 @@
 const { Op } = require("sequelize");
-const { 
-  Address, 
-  sequelize, 
-  User, 
-  StudentInformation, 
+const {
+  Address,
+  sequelize,
+  User,
+  StudentInformation,
   BasicEducation,
   TertiaryEducation,
-  ProfessionalSkill 
+  ProfessionalSkill
 } = require("../models");
 const { ApiError, ApiResp } = require("../utils/Response");
 
 const StudentController = {
-
   editStudentBasicInformation: async (req, res, next) => {
-
     const t = await sequelize.transaction();
 
     try {
       const { userId } = req.params;
 
-      const user = await User.findOne( 
-        { where: { id: userId }}, 
+      const user = await User.findOne(
+        { where: { id: userId } },
         { transaction: t }
       );
       const studentInfo = await StudentInformation.findOne(
-        { where: { userId: userId }},
+        { where: { userId: userId } },
         { transaction: t }
-      )
+      );
 
-      if(!user || !studentInfo) throw new ApiError('Error saving user basic information', 404);
+      if (!user || !studentInfo)
+        throw new ApiError("Error saving user basic information", 404);
 
-      await user.update({...req.body}, { transaction: t });
-      await studentInfo.update({...req.body}, { transaction: t })
+      await user.update({ ...req.body }, { transaction: t });
+      await studentInfo.update({ ...req.body }, { transaction: t });
 
-      console.log(user)
-      console.log(studentInfo)
+      console.log(user);
+      console.log(studentInfo);
 
-      await t.commit()
+      await t.commit();
 
-      return res.status(200).json(ApiResp('User basic information updated successfully'))
-
+      return res
+        .status(200)
+        .json(ApiResp("User basic information updated successfully"));
     } catch (e) {
-
       console.log(e);
       await t.rollback();
       next(e);
-
     }
   },
 
   addStudentAddress: async (req, res, next) => {
-
     try {
-
       const address = await Address.create({
         ...req.body,
         userId: req.user.id
@@ -61,19 +57,14 @@ const StudentController = {
       if (!address) throw new ApiError("Error saving address", 404);
 
       return res.status(201).json(ApiResp("Address saved successfully"));
-
     } catch (e) {
-
       console.log(e);
       next(e);
-
     }
   },
 
   editStudentAddress: async (req, res, next) => {
-    
-    try{
-
+    try {
       const { addressId } = req.params;
 
       const address = await Address.findOne({
@@ -85,143 +76,138 @@ const StudentController = {
       await address.update({ ...req.body });
 
       return res.status(200).json(ApiResp("Address updated successfully"));
-
-    }catch(e){
-      
-      console.log(e)
-      next(e)
-
+    } catch (e) {
+      console.log(e);
+      next(e);
     }
-
   },
 
   addBasicEducation: async (req, res, next) => {
-
-    try{
-
+    try {
       const basicEducation = await BasicEducation.create(req.body);
 
-      if(!basicEducation) throw new ApiError('Error saving basic aducation info', 404);
+      if (!basicEducation)
+        throw new ApiError("Error saving basic aducation info", 404);
 
-      return res.status(201).json(ApiResp('Basic education saved successfully'));
-
-    }catch(e){
-
-      console.log(e)
-      next(e)
-
+      return res
+        .status(201)
+        .json(ApiResp("Basic education saved successfully"));
+    } catch (e) {
+      console.log(e);
+      next(e);
     }
   },
 
   editBasicEducation: async (req, res, next) => {
+    try {
+      const { basicEducationId } = req.params;
 
-    try{
+      const basicEducation = await BasicEducation.findOne({
+        where: { id: basicEducationId }
+      });
 
-      const { basicEducationId } = req.params; 
-
-      const basicEducation = await BasicEducation.findOne({where: { id : basicEducationId }});
-
-      if(!basicEducation) throw new ApiError('Error updating basic education info', 404);
+      if (!basicEducation)
+        throw new ApiError("Error updating basic education info", 404);
 
       await basicEducation.update({ ...req.body });
 
-      return res.status(200).json(ApiResp('Basic education info updated successfully'));
-
-    }catch(e){
-
-      console.log(e)
-      next(e)
-
+      return res
+        .status(200)
+        .json(ApiResp("Basic education info updated successfully"));
+    } catch (e) {
+      console.log(e);
+      next(e);
     }
   },
 
   addTertiaryEducation: async (req, res, next) => {
-
-    try{
-
+    try {
       const tertiaryEducation = await TertiaryEducation.create({ ...req.body });
 
-      if(!tertiaryEducation) throw new ApiError('Error saving tertiary education info', 404);
+      if (!tertiaryEducation)
+        throw new ApiError("Error saving tertiary education info", 404);
 
-      return res.status(201).json(ApiResp('Tertiary education info saved successfully'));
-
-    }catch(e){
-
-      console.log(e)
-      next(e)
-
+      return res
+        .status(201)
+        .json(ApiResp("Tertiary education info saved successfully"));
+    } catch (e) {
+      console.log(e);
+      next(e);
     }
-
   },
 
   editTertiaryEducation: async (req, res, next) => {
-
-    try{
-
+    try {
       const { tertiaryEducationId } = req.params;
 
-      const tertiaryEducation = await TertiaryEducation.findOne({ where: { id: tertiaryEducationId } });
+      const tertiaryEducation = await TertiaryEducation.findOne({
+        where: { id: tertiaryEducationId }
+      });
 
-      if(!tertiaryEducation) throw new ApiError('Error updating tertiary education info');
+      if (!tertiaryEducation)
+        throw new ApiError("Error updating tertiary education info");
 
       await tertiaryEducation.update({ ...req.body });
 
-      return res.status(200).json(ApiResp('Tertiary education info updated successfully'));
-
-
-    }catch(e){
-
-      console.log(e)
-      next(e)
-
+      return res
+        .status(200)
+        .json(ApiResp("Tertiary education info updated successfully"));
+    } catch (e) {
+      console.log(e);
+      next(e);
     }
+  },
 
+  deleteTertiaryEducation: async (req, res, next) => {
+    try {
+      const { tertiaryEducationId } = req.params;
+
+      await TertiaryEducation.destroy({ where: { id: tertiaryEducationId } });
+
+      return res.status(200).json(ApiResp("Record deleted successfully"));
+    } catch (e) {
+      console.log(e);
+      next(e);
+    }
   },
 
   addProfessionalSkill: async (req, res, next) => {
-
-    try{
-
+    try {
       const professionalSkill = await ProfessionalSkill.create({ ...req.body });
 
-      if(!professionalSkill) throw new ApiError('Error saving professional skill', 404);
+      if (!professionalSkill)
+        throw new ApiError("Error saving professional skill", 404);
 
-      return res.status(201).json(ApiResp('Professional skill saved successfully'));
-
-
-    }catch(e){
-
-      console.log(e)
-      next(e)
-
+      return res
+        .status(201)
+        .json(ApiResp("Professional skill saved successfully"));
+    } catch (e) {
+      console.log(e);
+      next(e);
     }
-
   },
 
   editProfessionalSkill: async (req, res, next) => {
-
-    try{
-
+    try {
       const { professionalSkillId } = req.params;
 
-      const professionalSkill = await ProfessionalSkill.findOne({ where: { id: professionalSkillId } });
+      const professionalSkill = await ProfessionalSkill.findOne({
+        where: { id: professionalSkillId }
+      });
 
-      if(!professionalSkill) throw new ApiError('Error updating professional skill', 404);
+      if (!professionalSkill)
+        throw new ApiError("Error updating professional skill", 404);
 
       await professionalSkill.update({ ...req.body });
 
-      return res.status(200).json(ApiResp('Professional skill update successfully'));
-
-
-    }catch(e){
-
-      console.log(e)
-      next(e)
-
+      return res
+        .status(200)
+        .json(ApiResp("Professional skill update successfully"));
+    } catch (e) {
+      console.log(e);
+      next(e);
     }
-
   }
-
 };
 
 module.exports = StudentController;
