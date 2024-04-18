c28184689b35cf691942f3f8d6fcbf4c133ef6e7
Kamzen <37660966+Kamzen@users.noreply.github.com>
2024-04-18 11:25:12 +0200

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,8 @@ const express = require("express");
 const dotenv = require("dotenv");
 const morgan = require("morgan");
 const cors = require("cors");
-const cookieParser = require('cookie-parser')
+const cookieParser = require("cookie-parser");
+const fileUpload = require("express-fileupload");
 const { sequelize } = require("./models");
 const AppRouter = require("./routes");
 const ErrorMid = require("./middlewares/ErrorMid");
@@ -22,7 +23,8 @@ app.use(morgan("method :url :status :res[content-length] - :response-time ms"));
 app.use(cors({ origin: true, credentials: true }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser())
+app.use(cookieParser());
+app.use(fileUpload());
 
 const VERSION = process.env.API_VERSION || "v1";
 const API_BASE_URL = `/api/${VERSION}`;
@@ -31,13 +33,13 @@ const PORT = process.env.PORT || 8000;
 app.get("/", (req, res, next) => {
   return res.status(200).json({
     success: true,
-    message: "Yes, it is working",
+    message: "Yes, it is working"
   });
 });
 
 app.use(`${API_BASE_URL}`, AppRouter);
 
-app.use(ErrorMid)
+app.use(ErrorMid);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT} and version ${VERSION}`);
